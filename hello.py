c13113a634c0175406dfa649809f6ccd23fc5e85
ikatarasi <61139954+ikatarasi@users.noreply.github.com>
2021-02-18 15:39:54 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,13 @@
-import re
-str='abcde'
-match_obj=re.search('bcd',str)
-print(match_obj.group())
+import tkinter
+
+
+def btn_click():
+    print('ボタンがクリックされました')
+
+tki = tkinter.Tk()
+tki.geometry('300x200')
+tki.title('ボタンイベントの検証')
+
+btn = tkinter.Button(tki, text='計算', command=btn_click)
+btn.place(x=140, y=170)
+tki.mainloop()
