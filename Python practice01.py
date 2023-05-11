bf84f8398f7dca2d6bec23fba2066a4f2c3f405f
ikatarasi <61139954+ikatarasi@users.noreply.github.com>
2023-05-11 19:57:11 +0900

    Update Python practice01.py

--- Python practice01.py
+++ Python practice01.py
@@ -1,16 +1,23 @@
-class Animal:
-    
-    def __init__(self, name):
-        self.name = name
+import matplotlib.pyplot as plt
+import cv2
 
-    
-    def say(self):
-        print("こんにちは！　私は" + self.name + "です。")
+cascade_file = "haarcascade_frontalface_alt.xml"
+cascade = cv2.CascadeClassifier(cascade_file)
 
+img = cv2.imread("*********")
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-dog = Animal("いぬ")
-cat = Animal("ねこ")
+face_list = cascade.detectMultiScale(img_gray, minSize=(150,150))
 
+if len(face_list) == 0:
+    print("失敗")
+    quit()
 
-dog.say()
-cat.say()
+for (x,y,w,h) in face_list:
+    print("顔の座標=", x, y, w, h)
+    red = (0, 0, 255)
+    cv2.rectangle(img, (x, y), (x+w, y+h), red, thickness=20)
+
+cv2.imwrite("face-detect.png", img)
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.show()
